4b8c9c20a8990e7652e7a491ebc517b8fc609607
Fionabellcamp <fcampbell2001@gmail.com>
2021-12-15 22:38:20 -0500

    ButtonAnimation!

--- FA21ACTION_A/Assets/Scripts/PressurePadDoor.cs
+++ FA21ACTION_A/Assets/Scripts/PressurePadDoor.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class PressurePadDoor : MonoBehaviour
 {
+	public Animator anim;
 	
 	public bool onPlate = false;
+	
+	 void Start(){
+		anim = GetComponentInChildren<Animator>();
+		
+		
+      
+    }
 	
     public void OnCollisionEnter2D(Collision2D other) {
               if (other.gameObject.tag == "Player" || other.gameObject.tag == "Enemy") { //also add or enemey tag
                     
 					onPlate=true;
+					anim.SetTrigger("ButtonPushedDown");
+					anim.SetBool("isButtonDown", true);
 					
               }
 			  
@@ -20,6 +30,8 @@ public class PressurePadDoor : MonoBehaviour
               if (other.gameObject.tag == "Player" || other.gameObject.tag == "Enemy") { //also add or enemey tag
                     
 					onPlate=false;
+					anim.SetTrigger("ButtonGoingUp");
+					anim.SetBool("isButtonDown", false);
 					
               }
 			  
